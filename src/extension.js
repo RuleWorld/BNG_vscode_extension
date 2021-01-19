7d63d0dcf79355940881759881520db892e32f05
ASinanSaglam <asinansaglam@gmail.com>
2021-01-19 18:30:43 -0500

    extension can now make a folder and copy current file in

--- src/extension.js
+++ src/extension.js
@@ -12,21 +12,47 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "bng" is now active!');
+	// console.log('Congratulations, your extension "bng" is now active!');
 
+	const commandName = 'bng.helloWorld';
+
+	function commandHandler() {
+		// first we try to grab our terminal and create one if it doesn't exist
+		let term = vscode.window.terminals.find(i => i.name == "bngl_term");
+		if (term == undefined) {
+			term = vscode.window.createTerminal("bngl_term");
+		}
+		// TODO: Make sure we have perl in our environment or raise an 
+		// error 
+
+		// TODO: make sure we have BNGPATH in our terminal environment
+		// or raise an error
+
+		// next we make a folder friendly time stamp
+		// TODO: Update this, at the moment it just gives the current date in
+		// miliseconds
+		const fold_name = Date.now().toString();
+		// Get workspace URI 
+		let curr_workspace_uri = vscode.workspace.getWorkspaceFolder(vscode.window.activeTextEditor.document.uri).uri;
+		// Get folder URI to make the new folder
+		let new_fold_uri = vscode.Uri.joinPath(curr_workspace_uri, fold_name);
+		// Create new directory 
+		vscode.workspace.fs.createDirectory(new_fold_uri);
+		// get current file path
+		let curr_doc_uri = vscode.window.activeTextEditor.document.uri;
+		let copy_path = vscode.Uri.joinPath(new_fold_uri, "test.bngl");
+		// vscode.window.showInformationMessage(`copying from: ${curr_doc_uri.toString()} to ${copy_path.toString()}`)
+		vscode.workspace.fs.copy(curr_doc_uri, copy_path);
+		// TODO: and now we need the terminal to go into that folder
+		// and now run perl ${BNGPATH}/BNG2.pl ${CUR_BNGL_FILE}
+		term.sendText("perl -v");
+		// Done running, let the user know
+		vscode.window.showInformationMessage('Done running');
+	}
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('bng.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// we need to run the current file through 
-		// perl BNG2.pl curr_file.bngl
-		
-
-		// Done running, let the user know
-		vscode.window.showInformationMessage('Done running');
-	});
+	let disposable = vscode.commands.registerCommand(commandName, commandHandler);
 
 	context.subscriptions.push(disposable);
 }
